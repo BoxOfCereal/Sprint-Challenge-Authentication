e4dc84e83e3cd02c96e35c1a26a0f7fb95cf3921
Noah <noah.stoeckel@gmail.com>
2019-04-06 14:15:33 -0400

    completes Jokes component

--- challenge-front-end/src/Jokes/Jokes.js
+++ challenge-front-end/src/Jokes/Jokes.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import requiresAuth from "../auth/requiresAuth";
 
 const Jokes = () => {
+  const [data, setData] = useState([]);
   const URL = `http://localhost:3300/api/jokes`;
+
+  useEffect(() => {
+    //use effect does not support asynchronous functions
+    // because useEffect needs to return a clean and function or nothing
+    // but you can use an asynchronous function inside useEffect
+    const fetchData = async () => {
+      const result = await axios(URL);
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <h1>Jokes</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.joke}</li>
+        ))}
+      </ul>
     </>
   );
 };
