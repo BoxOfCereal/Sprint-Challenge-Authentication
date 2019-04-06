e8bb3858f10860fdd1d99338bb551b3a1f27c126
Noah <noah.stoeckel@gmail.com>
2019-04-06 12:37:54 -0400

    adds user model and registration

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const userModel = require("../database/users-model");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  //get credentials from body
+  let user = req.body;
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 10);
+  //replace the password with the hash
+  user.password = hash;
+
+  //at the user to the database
+  userModel
+    .add(user)
+    .then(u => {
+      res.status(201).json(u);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
@@ -18,15 +35,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
