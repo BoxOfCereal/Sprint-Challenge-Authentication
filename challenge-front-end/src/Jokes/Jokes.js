41e00d2661cdb3b2c7c2a67fd3c82df02a6ad073
Noah <noah.stoeckel@gmail.com>
2019-04-06 14:37:08 -0400

    add search term to the joke component

--- challenge-front-end/src/Jokes/Jokes.js
+++ challenge-front-end/src/Jokes/Jokes.js
@@ -4,22 +4,33 @@ import requiresAuth from "../auth/requiresAuth";
 
 const Jokes = () => {
   const [data, setData] = useState([]);
-  const URL = `http://localhost:3300/api/jokes`;
+  const [query, setQuery] = useState("hipster");
 
   useEffect(() => {
     //use effect does not support asynchronous functions
     // because useEffect needs to return a clean and function or nothing
     // but you can use an asynchronous function inside useEffect
+
+    //http://localhost:3300/api/jokes?term=hipster
+    const URL = `http://localhost:3300/api/jokes`;
     const fetchData = async () => {
-      const result = await axios(URL);
+      const result = await axios(`${URL}?term=${query}`);
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [query]); //the effect is now depending on the query
 
   return (
     <>
       <h1>Jokes</h1>
+      <label htmlFor="search-term">Search Term</label> <br />
+      <input
+        name="search-term"
+        id="search-term"
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
       <ul>
         {data.map(item => (
           <li key={item.id}>{item.joke}</li>
