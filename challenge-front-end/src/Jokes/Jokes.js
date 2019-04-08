944be81c822a617b19672e73e0087241b7e6c69e
Noah <noah.stoeckel@gmail.com>
2019-04-08 00:35:21 -0400

    Refactor Fetch Hook

--- challenge-front-end/src/Jokes/Jokes.js
+++ challenge-front-end/src/Jokes/Jokes.js
@@ -1,47 +1,44 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import requiresAuth from "../auth/requiresAuth";
+import useAxiosFetchData from "../customHooks/useAxiosFetchData";
 
 const Jokes = () => {
-  const [data, setData] = useState([]);
-  const [input, setInput] = useState("");
-  const [search, setSearch] = useState("hipster");
-
-  useEffect(() => {
-    //use effect does not support asynchronous functions
-    // because useEffect needs to return a clean and function or nothing
-    // but you can use an asynchronous function inside useEffect
-
-    //http://localhost:3300/api/jokes?term=hipster
-    const URL = `http://localhost:3300/api/jokes`;
-    const fetchData = async () => {
-      const result = await axios(`${URL}?term=${search}`);
-      setData(result.data);
-    };
-    fetchData();
-  }, [search]); //the effect is now depending on the search
+  const { data, isLoading, isError, doFetch } = useAxiosFetchData(
+    `http://localhost:3300/api/jokes`,
+    []
+  );
+  const [query, setQuery] = useState(""); //holds state of query term from input
 
   return (
     <>
       <h1>Jokes</h1>
+      {console.count("render")}
       <label htmlFor="search-term">Search Term</label> <br />
       {/* set search based off input */}
-      <button type="button" onClick={() => setSearch(input)}>
+      <button
+        type="button"
+        onClick={() => doFetch(`http://localhost:3300/api/jokes?term=${query}`)}
+      >
         Search
       </button>
       <input
         name="search-term"
         id="search-term"
         type="text"
-        value={input}
+        value={query}
         placeholder="hispter"
-        onChange={event => setInput(event.target.value)}
+        onChange={event => setQuery(event.target.value)}
       />
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.joke}</li>
-        ))}
-      </ul>
+      {isError && <div>Something went wrong ...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.joke}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
